4f28946a62c871118b84131e79a7f0f2afa8dfdd
AJDelgadillo <105257231+AJDelgadillo@users.noreply.github.com>
2022-07-08 14:28:35 -0700

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -51,9 +51,54 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-)
+	PRIMARY KEY (emp_no, title, from_date)
+);
 
-SELECT * FROM departments;
+DROP TABLE titles CASCADE
+
+SELECT * FROM titles;
+
+--- Retirement Eligability
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'
+
+--- Narrow down to employees that were hired between the years 1985 and 1988
+
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--- Show only the amount of employees that fall into these catagories
+
+SELECT COUNT (first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--- Creating a new table to hold the retirement eligability data
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info
+
 
 
